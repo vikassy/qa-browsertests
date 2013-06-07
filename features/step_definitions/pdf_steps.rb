4b7fbedcfc875081ecf8dfc7439000cd6a34fe68
Zeljko Filipin <zeljko.filipin@gmail.com>
2013-06-07 17:47:19 +0200

    Increase the time we wait for the link to appear
    Error message: timed out after 15 seconds, waiting for
    {:text=>"Download the file", :tag_name=>"a"} to become present
    (Watir::Wait::TimeoutError)
    
    Failed Jenkins job:
    https://wmf.ci.cloudbees.com/view/s-e.w-l/job/browsertests-en.wikipedia.
    beta.wmflabs.org-linux-chrome/103/testReport/(root)/PDF/Click_on_Downloa
    d_as_PDF_link/
    
    Change-Id: Ibbdce47cd1e8fccbc95eefaa9851250093e5159b

--- features/step_definitions/pdf_steps.rb
+++ features/step_definitions/pdf_steps.rb
@@ -5,7 +5,5 @@ When(/^I click on Download as PDF$/) do
   on(RandomPage).download_as_pdf
 end
 Then(/^Download the file link should be present$/) do
-  on(RandomPage) do |page|
-    page.download_the_file_element.when_present(15).should exist
-  end
+  on(RandomPage).download_the_file_element.when_present(30).should exist
 end
