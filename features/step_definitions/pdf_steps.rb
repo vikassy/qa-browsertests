ee47098c9ca78025e66db506a62dbdb9642edd9a
Zeljko Filipin <zeljko.filipin@gmail.com>
2013-06-07 18:43:09 +0200

    Make sure the link is there before trying to click it
    Fixes: Element is not currently visible and so may not be interacted
    with (Selenium::WebDriver::Error::ElementNotVisibleError)
    
    Change-Id: I478adec5f48d3aa484e00573ca22302fa6909602

--- features/step_definitions/pdf_steps.rb
+++ features/step_definitions/pdf_steps.rb
@@ -2,7 +2,7 @@ Then(/^Download as PDF should be present$/) do
   on(RandomPage).download_as_pdf_element.should exist
 end
 When(/^I click on Download as PDF$/) do
-  on(RandomPage).download_as_pdf
+  on(RandomPage).download_as_pdf_element.when_present.click
 end
 Then(/^Download the file link should be present$/) do
   on(RandomPage).download_the_file_element.when_present(30).should exist
