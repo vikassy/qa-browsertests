836e258914a276ad67dcf88484f189bca3492616
cmcmahon <cmcmahon@wikimedia.org>
2013-06-10 13:02:41 -0700

    IN PROCESS: test for uls cog behavior for anonymous users, please review
    Change-Id: If3da0d8c2fb8b622473198b9f22a09ef60183a20

--- features/step_definitions/uls_cog_sidebar_user_steps.rb
+++ features/step_definitions/uls_cog_sidebar_user_steps.rb
@@ -15,6 +15,12 @@ Given(/^I am on a talk page without interlanguage links$/) do
   visit(NoInterlanguagePage).talk_element.click
 end
 
+Given(/^I navigate to the anonymous Language Settings panel$/) do
+  step 'I am on a page with interlanguage links'
+  step 'I click the cog icon by Languages in the sidebar'
+  step 'I see the anonymous Language Settings panel'
+end
+
 Given(/^I navigate to the Language Settings panel$/) do
   step 'I am on a page with interlanguage links'
   step 'I click the cog icon by Languages in the sidebar'
@@ -65,6 +71,13 @@ Then(/^I can enable input methods$/) do
   on(InterlanguagePage).enable_input_methods_element.when_visible.should be_visible
 end
 
+Then(/^I can navigate back to Input Settings$/) do
+  on(InterlanguagePage) do |page|
+    page.back_to_input
+    page.x_element.should be_visible
+  end
+end
+
 Then(/^I can navigate back to Language Settings$/) do
   on(InterlanguagePage) do |page|
     page.back_to_display
@@ -93,6 +106,13 @@ Then(/^I see Language Search$/) do
   on(InterlanguagePage).language_search_element.should be_visible
 end
 
+Then(/^I see the anonymous Language Settings panel$/) do
+  on(NoInterlanguagePage) do |page|
+    page.language_button_element.when_present.should be_visible
+    page.fonts_button_element.should be_visible
+  end
+end
+
 Then(/^I see the Language Settings panel$/) do
   on(NoInterlanguagePage) do |page|
     page.language_button_element.when_present.should be_visible
@@ -107,6 +127,11 @@ Then(/^I see Worldwide$/) do
     page.language_list.should match Regexp.escape('Worldwide')
     page.english_link_element.should be_visible
   end
+end
+
+Then(/^the cog icon brings up anonymous Language Settings again$/) do
+  step 'I click the cog icon by Languages in the sidebar'
+  step 'I see the anonymous Language Settings panel'
 end
 
 Then(/^the cog icon brings up Language Settings again$/) do
