79e021bb72619653dadffc631ecdec58202e1f02
cmcmahon <cmcmahon@wikimedia.org>
2013-06-12 13:53:55 -0700

    Working code for June 27 tech demo meetup
    Change-Id: Ic49cd5268d2bd9c2357402f0b889328d0924486b

---
+++ features/support/pages/wikilove_page.rb
@@ -0,0 +1,20 @@
+require 'page-object'
+
+class WikilovePage
+  include PageObject
+
+  include URL
+  page_url URL.url('User:Selenium_user2')
+
+  select_list(:barnstar_select, id: 'mw-wikilove-subtype')
+  div(:barnstars, text: 'Barnstars')
+  div(:food_and_drink, text: 'Food and drink')
+  select_list(:food_drink_select, id: 'mw-wikilove-subtype')
+  text_field(:food_header, id: 'mw-wikilove-header')
+  a(:heart, text: 'WikiLove')
+  text_field(:kitten_header, id: 'mw-wikilove-header')
+  a(:kitten_image, id: 'mw-wikilove-gallery-img-0')
+  div(:kittens, text: 'Kittens')
+  text_area(:wikilove_message, id: 'mw-wikilove-message')
+  div(:wikilove_spinner, id: 'mw-wikilove-image-preview-spinner')
+end
