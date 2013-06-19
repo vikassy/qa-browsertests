c66bcef314a6199b6ccf743aa6233c67ae73cfd3
cmcmahon <cmcmahon@wikimedia.org>
2013-06-19 11:56:09 -0700

    make page.feature pass in envs where VE is enabled
    Change-Id: I3e8098601218ed6466cb55eeca1ff3ef50e06cb8

--- features/support/pages/does_not_exist_page.rb
+++ features/support/pages/does_not_exist_page.rb
@@ -4,6 +4,6 @@ class DoesNotExistPage
   include URL
   page_url URL.url('<%=params[:page_name]%>')
 
-  a(:create, text: 'Create')
+  a(:create, text: 'Create source')
   div(:page_text, id: 'mw-content-text')
 end
