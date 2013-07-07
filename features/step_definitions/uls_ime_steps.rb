37de4befc8da674750b29220fda084a902e1b59f
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2013-07-07 19:38:22 +0300

    A test for bug 50790
    This test checks that the selected input method
    is remembered after disabling IME and going to
    another page.
    
    Bug: 50790
    Change-Id: I94bd779577251d51a20471a6a39ab1ee6cd6927e

--- features/step_definitions/uls_ime_steps.rb
+++ features/step_definitions/uls_ime_steps.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 When(/^I click on an input box$/) do
   on(RandomPage).search_input_element.click
 end
@@ -16,4 +18,36 @@ end
 
 Then(/^I should see names of languages in it$/) do
   on(RandomPage).input_method_language_list_element.should be_visible
+end
+
+When(/^I click on the ellipsis at the bottom of the list$/) do
+  on(RandomPage).more_languages
+end
+
+When(/^in the language filter I type (.+)$/) do |language_abbreviation|
+  on(RandomPage).language_filter=language_abbreviation
+end
+
+When(/^I click on the link to select Malayalam$/) do
+  on(RandomPage).malayalam_link
+end
+
+When(/^I click on the Malayalam InScript 2 menu item$/) do
+  on(RandomPage).uls_malayalam_inscript2_item
+end
+
+When(/^I press Control\-M$/) do
+  on(RandomPage).search_input_element.send_keys [:control, 'm']
+end
+
+When(/^I go to another random page$/) do
+  visit(RandomPage)
+end
+
+Then(/^in it there must be an element with Malayalam text$/) do
+  on(RandomPage).input_method_enabled == 'ഇൻസ്ക്രിപ്റ്റ് 2'
+end
+
+Then(/^I should see the enabled input method keyboard icon at the bottom right$/) do
+  on(RandomPage).input_method_enabled_element.should be_visible
 end
--- features/uls_ime.feature
+++ features/uls_ime.feature
@@ -12,3 +12,19 @@ Feature: Universal Language Selector input method engine
       And I click on the IME keyboard icon
     Then I should see the dropdown menu with a list of keymaps
       And I should see names of languages in it
+
+  Scenario: Input method is preserved after going to a different page
+    Given I am at random page
+    When I click on an input box
+      And I click on the IME keyboard icon
+      And I click on the ellipsis at the bottom of the list
+      And in the language filter I type ml
+      And I click on the link to select Malayalam
+      And I click on the IME keyboard icon
+      And I click on the Malayalam InScript 2 menu item
+      And I press Control-M
+      And I go to another random page
+      And I click on an input box
+      And I press Control-M
+    Then I should see the enabled input method keyboard icon at the bottom right
+      And in it there must be an element with Malayalam text
