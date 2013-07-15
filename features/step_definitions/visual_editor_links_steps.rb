586291698eaf173162c84c66728b637df1f20d9d
rachel99 <rachelthomas_99@yahoo.com>
2013-07-15 10:05:40 -0400

    Check for internal link and link that doesn't exist
    Change-Id: I6300ad3364a453708eb348a2a43f72123983a661

--- features/step_definitions/visual_editor_links_steps.rb
+++ features/step_definitions/visual_editor_links_steps.rb
@@ -22,3 +22,29 @@ Then(/^an external link appears in the diff view$/) do
     page.diff_view.should match Regexp.escape('[http://www.example.com Editing] ')
   end
 end
+
+When(/^I click the blue text for Matching Page$/) do
+  on(VisualEditorPage).internal_linksuggestion_element.when_present.click
+end
+
+Then(/^an internal link appears in the diff view$/) do
+  on(VisualEditorPage) do |page|
+    page.wait_until(10) do
+      page.internal_diff_view.include? 'Main Page'
+    end
+    page.internal_diff_view.should match Regexp.escape('[[Main Page|Editing]]')
+  end
+end
+
+When(/^I click the blue text for New Page$/) do
+  on(VisualEditorPage).newpage_linksuggestion_element.when_present.click
+end
+
+Then(/^a non\-existing link appears in the diff view$/) do
+  on(VisualEditorPage) do |page|
+    page.wait_until(10) do
+      page.internal_diff_view.include? 'DoesNotExist'
+    end
+    page.internal_diff_view.should match Regexp.escape('[[DoesNotExist|Editing]]')
+  end
+end
--- features/visual_editor_links.feature
+++ features/visual_editor_links.feature
@@ -15,3 +15,21 @@ Feature: VisualEditor Links
     And I click Save page
     And I click Review your changes
   Then an external link appears in the diff view
+
+  Scenario: Enter internal link
+  Given I can see the Link User Inteface
+  When I enter Main Page into Content box
+    And I click the blue text for Matching Page
+    And I click < to close Link User Interface
+    And I click Save page
+    And I click Review your changes
+  Then an internal link appears in the diff view
+
+  Scenario: Enter non-existing link
+    Given I can see the Link User Inteface
+    When I enter DoesNotExist into Content box
+    And I click the blue text for New Page
+    And I click < to close Link User Interface
+    And I click Save page
+    And I click Review your changes
+    Then a non-existing link appears in the diff view
