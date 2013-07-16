06834c5d3db498a96cf0c7da8f273f8a90bc5b53
Zeljko Filipin <zeljko.filipin@gmail.com>
2013-07-16 16:33:49 +0200

    Scenario: Visual editor does not appear if user is not logged in
    Feature: VisualEditor
    
    I have paired on this with Rachel. For now we just check page URL to
    see if Visual Editor opens or not.
    
    Change-Id: I6216eb7c155b2ed4a31052691fa40f1dfa1533ec
    TODO: check page content to see if Visual editor opened or not.

--- features/step_definitions/visual_editor_steps.rb
+++ features/step_definitions/visual_editor_steps.rb
@@ -1,6 +1,9 @@
 Given(/^I am at my user page$/) do
   visit(VisualEditorPage)
 end
+Given(/^I am not logged in$/) do
+  # user is logged out by default, nothing to do here
+end
 
 When(/^I click Review and Save$/) do
   on(VisualEditorPage) do |page|
@@ -8,51 +11,47 @@ When(/^I click Review and Save$/) do
     page.review_and_save_element.when_visible.click
   end
 end
-
 When(/^I click Looks good to me$/) do
   on(VisualEditorPage) do |page|
     page.diff_view_element.when_visible.should be_visible
     page.looks_good_element.click
   end
 end
-
 When(/^I click This is a minor edit$/) do
   on(VisualEditorPage).minor_edit_element.when_present.click
 end
-
 When(/^I click Save page$/) do
   on(VisualEditorPage) do |page|
     sleep 2 # blame Chris for this!
     page.save_page_element.when_present.click
   end
 end
-
 When(/^I click Save page the second time$/) do
     on(VisualEditorPage).second_save_page_element.when_present.click
 end
-
 When(/^I edit the page with a string$/) do
   on(VisualEditorPage) do |page|
     page.edit_ve
     page.content_element.when_visible.send_keys("Editing with #{@does_not_exist_page_name}")
   end
 end
-
 When(/^I click Return to save form$/) do
   on(VisualEditorPage) do |page|
     page.diff_view_element.when_present
     page.return_to_save_element.when_present.click
   end
 end
-
 When(/^I click Review your changes$/) do
   on(VisualEditorPage).review_changes_element.when_present.click
 end
-
 When(/^I edit the description of the change$/) do
   on(VisualEditorPage).describe_change_element.when_visible.send_keys("Describing with #{@does_not_exist_page_name}")
 end
 
+Then(/^Edit Source page should open$/) do
+  @browser.url.should match /&action=edit/
+  @browser.url.should_not match /\?veaction=edit/
+end
 Then(/^Page text should contain the string$/) do
   on(VisualEditorPage).page_text_element.when_present.text.should match Regexp.escape("Editing with #{@does_not_exist_page_name}")
 end
--- features/visual_editor.feature
+++ features/visual_editor.feature
@@ -1,11 +1,16 @@
-@ie6-bug  @ie7-bug  @ie8-bug @test2.wikipedia.org @login
+@ie6-bug  @ie7-bug  @ie8-bug @test2.wikipedia.org
 Feature: VisualEditor
 
-  Background:
-    Given I am logged in
+  Scenario: Visual editor does not appear if user is not logged in
+    Given I am not logged in
+    When I am at random page
+      And I click the Edit link on the wiki page
+    Then Edit Source page should open
 
+  @login
   Scenario: Basic edit
-    Given I am at my user page
+    Given I am logged in
+      And I am at my user page
     When I edit the page with a string
       And I click Save page
       And I click This is a minor edit
