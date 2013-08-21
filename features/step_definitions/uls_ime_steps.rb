fc38fe9ed12f9e0ea2a72b6e70db9fceac15b6f7
Niklas Laxström <niklas.laxstrom@gmail.com>
2013-08-21 15:47:49 +0300

    Add tests for language name display in ime menu
    Some refactoring to make tests more expressive
    
    This commit depends on
    I4a6cc9a79f186941cc79533de90ab0fb7e104673
    
    Change-Id: Ib8796525dcdd679f5d78a40db3f561aa537a2096

--- features/step_definitions/uls_ime_steps.rb
+++ features/step_definitions/uls_ime_steps.rb
@@ -1,39 +1,58 @@
 # encoding: utf-8
 
-When(/^I click on an input box$/) do
-  on(RandomPage).search_input=" "
+Given(/^I am on a wiki in Kotava language$/) do
+  visit(RandomPage)
+  # Fake a Kotava Wiki
+  @browser.execute_script( "mw.config.set( 'wgContentLanguage', 'avk' )" )
 end
 
-When(/^I click on the IME keyboard icon$/) do
+When(/^I click on an input box$/) do
+  on(RandomPage) do |page|
+    page.search_input_element.click
+    # For some reason click alone doesn't seem to trigger the ime indicator
+    # when running tests
+    page.search_input_element.send_keys ' '
+  end
+end
+
+When(/^I click on the input method indicator$/) do
   on(RandomPage).input_method_element.click
 end
 
-Then(/^I should see the input method keyboard icon at the bottom right$/) do
+When(/^I open the input method menu$/) do
+  on(RandomPage) do |page|
+    page.search_input_element.click
+    page.search_input_element.send_keys ' '
+    page.input_method_element.click
+  end
+end
+
+Then(/^I should see the input method indicator$/) do
   on(RandomPage).input_method_element.should be_visible
 end
 
-Then(/^I should see the dropdown menu with a list of keymaps$/) do
+Then(/^I should see input methods for (.+)/) do |language|
+  on(RandomPage).input_method_ime_list_title.should == language
+end
+
+Then(/^I should see a list of available input methods$/) do
   on(RandomPage).input_method_selector_menu_element.should be_visible
 end
 
-Then(/^I should see names of languages in it$/) do
+Then(/^I should see a list of suggested languages$/) do
   on(RandomPage).input_method_language_list_element.should be_visible
 end
 
-When(/^I click on the ellipsis at the bottom of the list$/) do
-  on(RandomPage).more_languages
-end
-
-When(/^in the language filter I type (.+)$/) do |language_abbreviation|
-  on(RandomPage).language_filter=language_abbreviation
-end
-
-When(/^I click on the link to select Malayalam$/) do
-  on(RandomPage).malayalam_link
+When(/^I choose (.+?) as the input language$/) do |language|
+  on(RandomPage) do |page|
+    page.more_languages
+    page.language_filter = language
+    page.language_filter_element.send_keys :return
+  end
 end
 
 When(/^I click on the Malayalam InScript 2 menu item$/) do
-  on(RandomPage).uls_malayalam_inscript2_item
+  on(RandomPage).uls_malayalam_inscript2_item_element.click
 end
 
 When(/^I press Control\-M$/) do
@@ -45,9 +64,6 @@ When(/^I go to another random page$/) do
 end
 
 Then(/^in it there must be an element with Malayalam text$/) do
-  on(RandomPage).input_method_enabled == 'ഇൻസ്ക്രിപ്റ്റ് 2'
-end
-
-Then(/^I should see the enabled input method keyboard icon at the bottom right$/) do
-  on(RandomPage).input_method_enabled_element.should be_visible
+  # 'input_method_enabled' alone only returns []
+  on(RandomPage).input_method_enabled_element.text.should == 'ഇൻസ്ക്രിപ്റ്റ് 2'
 end
--- features/uls_ime.feature
+++ features/uls_ime.feature
@@ -1,30 +1,51 @@
-@test2.wikipedia.org @sandbox.translatewiki.net
+@commons.wikimedia.beta.wmflabs.org
 Feature: Universal Language Selector input method engine
 
-  Scenario: User can see input method keyboard icon
-    Given I am at random page
-    When I click on an input box
-    Then I should see the input method keyboard icon at the bottom right
+User is able to select among different input methods via a menu which is
+activated by the input method indicator. User can type with selected input
+method in different input fields and temporarily activate and deactivate the
+input method.
 
-  Scenario: User can see a dropdown menu when they click on the IME icon
-    Given I am at random page
-    When I click on an input box
-      And I click on the IME keyboard icon
-    Then I should see the dropdown menu with a list of keymaps
-      And I should see names of languages in it
+  Scenario: Input method indicator is shown
 
-  Scenario: Input method is preserved after going to a different page
+  The input method indicator is shown when input field gets a focus.
+
     Given I am at random page
     When I click on an input box
-      And I click on the IME keyboard icon
-      And I click on the ellipsis at the bottom of the list
-      And in the language filter I type ml
-      And I click on the link to select Malayalam
-      And I click on the IME keyboard icon
+    Then I should see the input method indicator
+
+  Scenario: Input method menu
+
+  Input method menu is shown when user clicks the input method indicator.
+
+    Given I am at random page
+    When I click on an input box
+      And I click on the input method indicator
+    Then I should see input methods for English
+      And I should see a list of available input methods
+      And I should see a list of suggested languages
+
+  Scenario: Unsupported input language
+
+  User visits a wiki with content language that does not have have an input
+  method.
+
+    Given I am on a wiki in Kotava language
+    When I open the input method menu
+    Then I should see input methods for Kotava
+
+  Scenario: Sticky input methods
+
+  Chosen input method selection persists across page loads.
+
+    Given I am at random page
+    When I open the input method menu
+      And I choose ml as the input language
+      And I open the input method menu
       And I click on the Malayalam InScript 2 menu item
       And I press Control-M
       And I go to another random page
       And I click on an input box
       And I press Control-M
-    Then I should see the enabled input method keyboard icon at the bottom right
+    Then I should see the input method indicator
       And in it there must be an element with Malayalam text
