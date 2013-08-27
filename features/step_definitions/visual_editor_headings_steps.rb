b3dee325ca63563a9c8302f1188e2eecbca8fe09
Cmcmahon <cmcmahon@wikimedia.org>
2013-08-27 14:28:50 +0000

    Revert "WIP  Refactor VE headings scenarios"
    This reverts commit a614172462d2eec5951b89ae6ce4af1a80fde43b.
    
    Change-Id: I56fc13106e1826a1fc2352985bd2673bba618e85

--- features/step_definitions/visual_editor_headings_steps.rb
+++ features/step_definitions/visual_editor_headings_steps.rb
@@ -1,4 +1,12 @@
-When(/^I click the down arrow on Headings interface$/) do
+When(/^I click the Headings pull\-down menu$/) do
+   on(VisualEditorPage).ve_heading_menu_element.when_present.click
+ end
+
+Given(/^I can see the Headings interface$/) do
+  on(VisualEditorPage).ve_heading_ui.should match Regexp.escape('Paragraph')
+end
+
+When(/^I click the down arrow$/) do
   on(VisualEditorPage).downarrow_element.when_present.click
 end
 
@@ -13,10 +21,6 @@ Then(/^a paragraph should appear in the diff view$/) do
     end
     page.paragraph_diff_view.should match Regexp.escape('Editing with ')
   end
-end
-
-Then(/^I should be able to click the up arrow on the save box$/) do
-  on(VisualEditorPage).uparrow_element.when_present.click
 end
 
 When(/^I click Heading$/) do
@@ -58,54 +62,3 @@ Then(/^a sub\-heading2 should appear in the diff view$/) do
   end
 end
 
-When(/^I click Sub\-Heading3$/) do
-  on(VisualEditorPage).subheading3_element.when_present.click
-end
-
-Then(/^a sub\-heading3 should appear in the diff view$/) do
-  on(VisualEditorPage) do |page|
-    page.wait_until(10) do
-      page.heading_diff_view.include? '=====Editing '
-    end
-    page.heading_diff_view.should match Regexp.escape('=====Editing with ')
-  end
-end
-
-When(/^I click Sub\-Heading4$/) do
-  on(VisualEditorPage).subheading4_element.when_present.click
-end
-
-Then(/^a sub\-heading4 should appear in the diff view$/) do
-  on(VisualEditorPage) do |page|
-    page.wait_until(10) do
-      page.heading_diff_view.include? '======Editing '
-    end
-    page.heading_diff_view.should match Regexp.escape('======Editing with ')
-  end
-end
-
-When(/^I click Preformatted$/) do
-  on(VisualEditorPage).preformatted_element.when_present.click
-end
-
-Then(/^a Preformatted should appear in the diff view$/) do
-  on(VisualEditorPage) do |page|
-    page.wait_until(10) do
-      page.heading_diff_view.include? ' Editing '
-    end
-    page.heading_diff_view.should match Regexp.escape(' Editing with ')
-  end
-end
-
- When(/^I click Page title$/) do
-  on(VisualEditorPage).page_title_element.when_present.click
-end
-
-Then(/^a Page title should appear in the diff view$/) do
-  on(VisualEditorPage) do |page|
-    page.wait_until(10) do
-      page.heading_diff_view.include? '=Editing '
-    end
-    page.heading_diff_view.should match Regexp.escape('=Editing with ')
-  end
-end
