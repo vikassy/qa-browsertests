51a4a7c28b2c4eac383aee6ca4617c953c6f123d
cmcmahon <cmcmahon@wikimedia.org>
2013-10-09 07:57:11 -0600

    id value for Search button changed
    Change-Id: Ia191b881871010499622fc34abf65e41adcf5143

--- features/support/pages/search_page.rb
+++ features/support/pages/search_page.rb
@@ -2,7 +2,7 @@ class SearchPage
   include PageObject
 
   div(:one_result, class: 'suggestions-result')
-  button(:search_button, id: 'mw-searchButton')
+  button(:search_button, id: 'searchButton')
   text_field(:search_input, id: 'searchInput')
   div(:search_results, class: 'suggestions-results')
 end
